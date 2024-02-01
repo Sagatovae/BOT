345902fb39dc9956b66cad79be91038b1f67be2d
yaroslav <yrtplay@gmail.com>
2024-02-01 20:10:13 +0300

    Co-authored-by: dade55@rambler.ru <dade55@rambler.ru>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'TOKEN': 'MTIwMTYxOTk3ODI1MjczNDQ3NA.GwjIid.nasxiiMvUX7CWUeABPVzU8FvoZQeY3kYqcs054',
+    'TOKEN': 'MTIwMTYxOTk3ODI1MjczNDQ3NA.GXoWXh.A29ZUbVIM2RAtgZUyCme0gPEdWkJAlNaSQi4Do',
     'NAME BOT': 'roulette',
     'ID': '1201619978252734474',
     'PREFIX': '!'
